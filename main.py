6e2c6d366335b9276a3891488b0ee5178dafd4be
Ismy Fana Fillah <88079312+ismyfanafillah173@users.noreply.github.com>
2023-12-10 14:11:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from search import UndirectedGraph, GraphProblem, best_first_graph_search, astar_search
+from items import items
+from map import map_greedy, map_astar
+
+# Get input from user
+print("================================================================================================================================")
+print("                                               ████▓▒░▓▒░⡷⠂pickNship⠐⢾░▒▓░▒▓████                                               ")
+print("================================================================================================================================")
+print("Welcome to the freightage service!")
+print("Please enter the following information:")
+truck_capacity = int(input("Enter truck capacity (in kg): "))
+start_location = input("Enter start location: ")
+end_location = input("Enter end location: ")
+est_time = input("Enter estimated time (e.g. '01-12-23'): ")
+print("generate....")
+
+# Find the shortest route using greedy algorithm
+problem_greedy = GraphProblem(start_location, end_location, map_greedy)
+route_greedy = [node.state for node in best_first_graph_search(problem_greedy, lambda node: node.path_cost).path()]
+
+# Find the shortest route using A* algorithm
+problem_astar = GraphProblem(start_location, end_location, map_astar)
+route_astar = [node.state for node in astar_search(problem_astar).path()]
+
+# Sort items based on location and estimated time
+sorted_items = sorted(items, key=lambda x: (x['location'] == end_location, x['est_time'], x['weight']))
+
+# Select items that can fit in the truck using greedy algorithm
+total_weight = 0
+selected_items = []
+for item in sorted_items:
+    if item['location'] in route_greedy and item['est_time'] == est_time:
+        if total_weight + item['weight'] <= truck_capacity:
+            selected_items.append(item)
+            total_weight += item['weight']
+
+# Print results
+print("Results:")
+print(f"Truck capacity: {truck_capacity} kg")
+print(f"Estimated time: {est_time}")
+print(f"Route astar: {route_astar}")
+print(f"Route greedy: {route_greedy}")
+print(f"Selected items ({len(selected_items)} items, {total_weight} kg):")
+for item in selected_items:
+    print(f"- {item['name']} ({item['location']}) : {item['weight']} kg")
+print(f"Total weight: {total_weight} kg")
+print("Thank you for using our service!")
+print("================================================================================================================================")
+
---
+++ map.py
@@ -0,0 +1,59 @@
+from search import UndirectedGraph, GraphProblem, best_first_graph_search, astar_search
+
+# Define the map and its locations
+map_greedy = UndirectedGraph(dict(
+  BandaAceh = dict(Medan = 76.44),
+  Medan = dict(BandaAceh = 76.44, Pekanbaru = 83.13, Padang = 100.00),
+  Padang = dict(Medan = 100.00, Pekanbaru = 27.25, Jambi = 62.19, Bengkulu = 65.18),
+  Pekanbaru = dict(Medan = 83.13, Padang = 27.25, Jambi = 56.64),
+  TanjungPinang = dict(PangkalPinang = 67.02),
+  Jambi = dict(Pekanbaru = 56.64, Padang = 62.19, Palembang = 27.50, Bengkulu = 45.76),
+  Bengkulu = dict(Padang = 65.18, Jambi = 45.76, Palembang = 47.48, BandarLampung = 65.92),
+  Palembang = dict(Jambi = 27.50, PangkalPinang = 22.57, Bengkulu = 47.48, BandarLampung = 44.64),
+  BandarLampung = dict(Palembang = 44.64, Bengkulu = 65.92, Serang = 11.36),
+  PangkalPinang = dict(TanjungPinang = 67.02, Palembang = 22.57),
+  Serang = dict(BandarLampung = 11.36, Jakarta = 1.00),
+  Jakarta = dict(Serang = 1.00, Bandung = 10.48),
+  Bandung = dict(Jakarta = 10.48, Semarang = 51.25, Yogyakarta = 52.81),
+  Semarang = dict(Bandung = 51.25, Yogyakarta = 3.92, Surabaya = 39.70),
+  Yogyakarta = dict(Bandung = 52.81, Semarang = 3.92, Surabaya = 42.36),
+  Surabaya = dict(Semarang = 39.70, Yogyakarta = 42.36)
+  ))
+
+map_astar = UndirectedGraph(dict(
+  BandaAceh = dict(Medan = 47.43),
+  Medan = dict(BandaAceh = 47.43, Pekanbaru = 54.05, Padang = 79.35),
+  Padang = dict(Medan = 79.35, Pekanbaru = 26.77, Jambi = 53.50, Bengkulu = 51.45),
+  Pekanbaru = dict(Medan = 54.05, Padang = 26.77, Jambi = 40.25),
+  TanjungPinang = dict(PangkalPinang = 100.00),
+  Jambi = dict(Pekanbaru = 40.25, Padang = 53.50, Palembang = 22.75, Bengkulu = 41.04),
+  Bengkulu = dict(Padang = 51.45, Jambi = 41.04, Palembang = 40.96, BandarLampung = 60.67),
+  Palembang = dict(Jambi = 22.75, PangkalPinang = 29.14, Bengkulu = 40.96, BandarLampung = 15.74),
+  BandarLampung = dict(Palembang = 15.74, Bengkulu = 60.67, Serang = 8.33),
+  PangkalPinang = dict(TanjungPinang = 100.00, Palembang = 29.14),
+  Serang = dict(BandarLampung = 8.33, Jakarta = 1.00),
+  Jakarta = dict(Serang = 1.00, Bandung = 4.39),
+  Bandung = dict(Jakarta = 4.39, Semarang = 15.66, Yogyakarta = 32.53),
+  Semarang = dict( Bandung = 15.66, Yogyakarta = 3.84, Surabaya = 13.30),
+  Yogyakarta = dict(Bandung = 32.53, Semarang = 3.84, Surabaya = 14.32),
+  Surabaya = dict(Semarang = 13.30, Yogyakarta = 14.32)
+  ))
+
+map_astar.locations = dict(
+    BandaAceh = (5.548256326794867, 95.32375203957695),
+    Medan = (3.595191911928496, 98.67222196112931),
+    Palembang = (-2.9760750389921697, 104.7754307504188),
+    Padang = (-0.9470848568696755, 100.417183356826),
+    Bengkulu = (-3.7928444639239927, 102.26076463040094),
+    Pekanbaru = (0.5070613092125984, 101.44777749360388),
+    TanjungPinang = (0.9185469029856769, 104.46650731024035),
+    Jambi = (-1.6101257907018853, 103.61312225472736),
+    BandarLampung = (-5.447879783268549, 105.2652870845391),
+    PangkalPinang = (-2.1316287170897232, 106.11692924995657),
+    Serang = (-6.116933092322672, 106.15385208445778),
+    Jakarta = (-6.194451015875306, 106.82292091205278),
+    Bandung = (-6.917467204609942, 107.61912582176764),
+    Semarang = (-7.005147994398289, 110.43812581525368),
+    Yogyakarta = (-7.795582204419959, 110.3694911043283),
+    Surabaya = (-7.257473307124247, 112.75208772044523),
+)
